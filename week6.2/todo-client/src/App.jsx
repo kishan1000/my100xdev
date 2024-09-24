d2724c7c294de1f9e57a07a3804d867028a6db0d
Kishan <92583766+kishan1000@users.noreply.github.com>
2024-09-24 20:11:56 +0530

    changes in week6.2/todo-client/App.jsx

--- week6.2/todo-client/src/App.jsx
+++ week6.2/todo-client/src/App.jsx
@@ -4,6 +4,8 @@ import './App.css'
 
 function App() {
   const [todos, setTodos] = useState([]);
+  const [todoId, setTodoId] = useState(undefined);
+  const [singleTodo, setSingleTodo] = useState({});
 
   useEffect(() => {
     console.log("Back-end hit")
@@ -14,8 +16,21 @@ function App() {
       })
   }, [])
 
+  useEffect(()=>{
+    fetch(`http://localhost:3000/todo?id=${todoId}`).then((res)=>{
+      res.json().then((json)=>{
+        setSingleTodo(json)
+      })
+    })
+  }, [todoId])
+
   return (
     <>
+      <h1>Get todo</h1>
+      <TodoForm setTodoId={setTodoId}></TodoForm>
+      {/* <TodoById id={todoId} setSingleTodo={setSingleTodo}></TodoById> */}
+      <Todo key={singleTodo.id} title={singleTodo.title} description={singleTodo.description} completed={singleTodo.completed}></Todo>
+      <h1>All Todos</h1>
       {todos.map(todo => <Todo key={todo.id} title={todo.title} description={todo.description} completed={todo.completed}></Todo>)}
       {/* <Todo title={"jnbfd"} description={"fkjdg"} completed={true}></Todo> */}
     </>
@@ -23,6 +38,7 @@ function App() {
 }
 
 function Todo({ title, description, completed }) {
+  if(!title) return <></>;
   let completedText = "";
   if (completed) {
     completedText = "Completed";
@@ -34,6 +50,18 @@ function Todo({ title, description, completed }) {
       <h1>{title}</h1>
       <h4>{description}</h4>
       <button>{completedText}</button>
+    </div>
+  )
+}
+
+function TodoForm({ setTodoId }) {
+  function todoformhandler() {
+    setTodoId(document.getElementById("todoid").value);
+  }
+  return (
+    <div>
+      <input type="text" name="todoid" id="todoid" onKeyDownCapture={todoformhandler}/>
+      <button onClick={todoformhandler}>Get Todo</button>
     </div>
   )
 }
